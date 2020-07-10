b0e02738d3d35e929c3645421c189b75be86da5a
Spartans08 <65037029+Spartans08@users.noreply.github.com>
2020-07-10 18:48:30 -0400

    Add files via upload

---
+++ week2/if_statement_olympics.js
@@ -0,0 +1,87 @@
+if("5 < 3" || "cat" == 3){
+    console.log("Five is greater than Three","Cat is the length of 3")
+  }
+  if("cat" === "dog"){
+    console.log(" are the same")
+  } else {
+    console.log("not the same")
+  }
+  
+  var person = "B"
+  var age = 12
+  
+  if(age < 18){
+    console.log("not allowed in movie")
+  } else {
+    console.log("allowed in movie")
+  }
+  if(person === "B"){
+    console.log("allowed in movie")
+  }
+  if(person === "B" && age < 18){
+    console.log("not allowed in movie")
+  } else (
+    console.log("allowed in movie")
+  )
+
+  if(1 === "1"){
+    console.log("Strict")
+  } else if ("1" == 1){
+    console.log("Loose or Abstract")
+  } else {
+      console.log("not equal at all")
+  }
+
+  if(1 >= 2 && 2 == 4){
+    console.log("yes")
+  } else {
+    console.log("yes")
+  }
+
+  var booleanValue = true;
+  var stringValue = "dog"
+
+  if(booleanValue = true)
+    console.log("boolean")
+  if(stringValue = "dog")
+    console.log("dog is a string")
+  if(booleanValue) {
+    console.log("variable is defined");
+  } else {
+    console.log("variable not defined")
+  }
+  
+  var s = 13;
+  var b = 7;
+  var c = 27
+
+  if(s > 12){
+    console.log("s is greater than 12");
+  } else {
+    console.log("s is not greater than 12")
+  }
+
+  if(b > 12){
+    console.log("s is greater than 12");
+  } else {
+    console.log("s is not greater than 12")
+  }
+  
+  if(c > 12){
+    console.log("s is greater than 12");
+  } else {
+    console.log("s is not greater than 12")
+  }
+
+  var isEven = (2)
+
+  if(isEven === 2){
+    console.log("is even")
+    } else {
+        console.log("is odd")
+    }
+  if(isEven === 3){
+    console.log("is even")
+    } else {
+        console.log("is odd")
+    }
